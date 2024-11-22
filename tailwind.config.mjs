398b92629738460149ca39730c123d510a2eaa4b
valentinsalazar <facundev@outlook.com>
2024-11-22 06:18:28 -0300

    haciendo algun q otro cambiesito

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       screens: {
-        'xs': '360px', // Celulares pantalla muy chica
-        'sm': '440px', // Celulares
-        'md': '768px', // Celulares Horizontal y Tablet Vertical
-        'lg': '1024px', // Tablet Horizontal y Laptops
-        'xl': '1280px', // Laptops y escritorios chicos
-        '2xl': '1536px', // Escritorios grandes
+        xs: "360px", // Celulares pantalla muy chica
+        sm: "440px", // Celulares
+        md: "768px", // Celulares Horizontal y Tablet Vertical
+        lg: "1024px", // Tablet Horizontal y Laptops
+        xl: "1280px", // Laptops y escritorios chicos
+        "2xl": "1536px", // Escritorios grandes
       },
       colors: {
-        'firstColor': '#0E1B29',
-        'secondColor': "#FFED89",
-        'thirdColor': '#f3f3f3',
+        firstColor: "#0E1B29",
+        secondColor: "#FFED89",
+        thirdColor: "#f3f3f3",
       },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-        'bebas': ['Bebas Neue'],
+        sans: ["Poppins", "sans-serif"],
+        bebas: ["Bebas Neue"],
       },
       backgroundImage: {
-        'dager': 'url("/dager2.webp")',
-        'mainImage': "url('/dager2.webp')",
-      }
+        dager: 'url("/dager2.webp")',
+        mainImage: "url('/dager2.webp')",
+      },
     },
   },
   plugins: [],
-}
+};
